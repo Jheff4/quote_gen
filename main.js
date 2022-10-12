887bc4ad35cbd2b99b07a62707276ad5cd4f254b
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-10-12 23:02:52 +0100

    made loading time a lil more sophisticated

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 // Variables
 
 let btn = document.querySelector('#new-quote');
+let mainContainer = document.querySelector('.main-content');
 let quote = document.querySelector('.quote');
 let person = document.querySelector('.person');
 
 function happen() {
+  mainContainer.style.backgroundColor = '#dfdede';
   const load = document.createElement("div");
   load.classList.add('loader');
   quote.appendChild(load);
 }
 
 btn.addEventListener('click', function () {
-  happen()
+  happen();
+  
   fetch("https://type.fit/api/quotes")
     .then(response => response.json() )
     .then(response => {
+      mainContainer.style.backgroundColor = '#fff';
       let randomNum = Math.floor(Math.random() * response.length);
       quote.innerText = response[randomNum].text;
       person.innerText = response[randomNum].author;
@@ -25,8 +29,7 @@ btn.addEventListener('click', function () {
     // }
     // ))
     .catch(err => {
-      
-
+        mainContainer.style.backgroundColor = '#fff';
         quote.innerText = '';
         person.innerText = '';
         const message = "Connect to the internet and try again!"
